b599d9da0a76d105a6892402711d4b952b47109f
Dennis Tobar <dennis.tobar@gmail.com>
2016-04-15 22:56:09 -0300

    Creando primeros esbozos del resumen por fase

--- web/index.php
+++ web/index.php
@@ -10,8 +10,22 @@ $fat->route('GET /', function(\Base $fat){
     echo \Template::instance()->render('page.html');
 });
 
-$fat->route('GET /fsa/*', function(\Base $fat){
+$fat->route('GET /fsa/@num', function(\Base $fat){
     $fat->mset(['contenido' => 'fsa.html', 'titulo' => 'Fundación Salvador Allende']);
+    $num = $fat->get('PARAMS.num');
+    $json = json_decode(file_get_contents('../data/fsa.json'));
+    $resumen = $json->{$num}->resumen;
+    unset($json->{$num}->resumen);
+    $fat->mset(['archivos' => $json->{$num}, 'resumen' => $resumen]);
+    echo \Template::instance()->render('page.html');
+});
+
+$fat->route('GET /fsa/@num/@discurso', function(\Base $fat){
+    $fat->mset(['contenido' => 'fsa.html', 'titulo' => 'Fundación Salvador Allende']);
+    $num = $fat->get('PARAMS.num');
+    $discurso = str_replace(" ", "_", $fat->get('PARAMS.discurso'));
+    $json = json_decode(file_get_contents("../data/fsa/{$discurso}.json"));
+    $fat->set('archivos', $json->{$num});
     echo \Template::instance()->render('page.html');
 });
 
