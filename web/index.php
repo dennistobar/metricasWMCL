a1be95fdea72942f55b78439f877380438b3af51
Dennis Tobar <dennis.tobar@gmail.com>
2016-04-15 23:52:11 -0300

    Creando el detalle de los discursos

--- web/index.php
+++ web/index.php
@@ -5,12 +5,12 @@ require_once '../vendor/autoload.php';
 $fat = \Base::instance();
 $fat->config('../config.ini');
 
-$fat->route('GET /', function(\Base $fat){
+$fat->route('GET /', function (\Base $fat) {
     $fat->mset(['contenido' => 'fsa.html']);
     echo \Template::instance()->render('page.html');
 });
 
-$fat->route('GET /fsa/@num', function(\Base $fat){
+$fat->route('GET /fsa/@num', function (\Base $fat) {
     $fat->mset(['contenido' => 'fsa.html', 'titulo' => 'Fundación Salvador Allende']);
     $num = $fat->get('PARAMS.num');
     $json = json_decode(file_get_contents('../data/fsa.json'));
@@ -20,12 +20,15 @@ $fat->route('GET /fsa/@num', function(\Base $fat){
     echo \Template::instance()->render('page.html');
 });
 
-$fat->route('GET /fsa/@num/@discurso', function(\Base $fat){
-    $fat->mset(['contenido' => 'fsa.html', 'titulo' => 'Fundación Salvador Allende']);
+$fat->route('GET /fsa/@num/@discurso', function (\Base $fat) {
+    $fat->mset(['contenido' => 'fsa-detalle.html', 'titulo' => 'Fundación Salvador Allende']);
     $num = $fat->get('PARAMS.num');
-    $discurso = str_replace(" ", "_", $fat->get('PARAMS.discurso'));
+    $discurso = str_replace(' ', '_', $fat->get('PARAMS.discurso'));
     $json = json_decode(file_get_contents("../data/fsa/{$discurso}.json"));
-    $fat->set('archivos', $json->{$num});
+    $visitas = (array)$json->visitas;
+    ksort($visitas);
+    $anos = array_unique(array_map(function($f){ return substr($f, 0, 4); }, array_keys($visitas)));
+    $fat->mset(['elemento' => $json, 'meses' => $visitas, 'anos' => $anos]);
     echo \Template::instance()->render('page.html');
 });
 
