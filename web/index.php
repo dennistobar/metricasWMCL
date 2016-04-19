9bdd7eded71415d2ebedef3c9023b7db2a3b328d
Dennis Tobar <dennis.tobar@gmail.com>
2016-04-19 17:19:43 -0300

    Añadiendo página inicial

--- web/index.php
+++ web/index.php
@@ -12,6 +12,18 @@ $fat->route('GET /', function (\Base $fat) {
 
 $fat->route('GET /fsa', function (\Base $fat) {
     $fat->mset(['contenido' => 'fsa/fsa-resumen.html']);
+    $json = json_decode(file_get_contents('../data/fsa.json'));
+    $resumen = [];
+    for ($i = 1; $i < 4; ++$i) {
+        $resumen[$i] = $json->{$i}->resumen;
+    }
+    $fat->mset(['resumen' => $resumen,
+        'total.paginas' => array_sum(array_map(function ($f) {return $f->paginas;}, $resumen)),
+        'total.discursos' => array_sum(array_map(function ($f) {return $f->discursos;}, $resumen)),
+        'total.mes_anterior' => array_sum(array_map(function ($f) {return $f->mes_anterior;}, $resumen)),
+        'total.mes_actual' => array_sum(array_map(function ($f) {return $f->mes_actual;}, $resumen)),
+        'total.visitas' => array_sum(array_map(function ($f) {return $f->visitas;}, $resumen)),
+    ]);
     echo \Template::instance()->render('page.html');
 });
 
@@ -30,9 +42,9 @@ $fat->route('GET /fsa/@num/@discurso', function (\Base $fat) {
     $num = $fat->get('PARAMS.num');
     $discurso = str_replace(' ', '_', $fat->get('PARAMS.discurso'));
     $json = json_decode(file_get_contents("../data/fsa/{$discurso}.json"));
-    $visitas = (array)$json->visitas;
+    $visitas = (array) $json->visitas;
     ksort($visitas);
-    $anos = array_unique(array_map(function($f){ return substr($f, 0, 4); }, array_keys($visitas)));
+    $anos = array_unique(array_map(function ($f) { return substr($f, 0, 4); }, array_keys($visitas)));
     $fat->mset(['elemento' => $json, 'meses' => $visitas, 'anos' => $anos]);
     echo \Template::instance()->render('page.html');
 });
